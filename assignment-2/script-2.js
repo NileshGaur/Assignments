e46542dd4f0ed81d0d4d79ada9661f4ff5d04c95
NileshGaur <53678804+NileshGaur@users.noreply.github.com>
2024-05-26 19:48:38 +0530

    feat: search button

--- assignment-2/script-2.js
+++ assignment-2/script-2.js
@@ -1,27 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
   const productContainer = document.getElementById("product-container");
+  const searchBar = document.getElementById("search-bar");
+  const searchButton = document.getElementById("search-button");
+  let products = [];
 
   fetch("https://dummyjson.com/products")
     .then((response) => response.json())
     .then((data) => {
-      data.products.forEach((product) => {
-        const productCard = document.createElement("div");
-        productCard.className = "product-card";
-
-        productCard.innerHTML = `
-            <img src="${product.thumbnail}" alt="${product.title}">
-            <div class="product-info">
-              <h2>${product.title}</h2>
-              <p class="description">${product.description}</p>
-              <p class="price">$${product.price}</p>
-              <p class="rating">Rating: ${product.rating} &#9733;</p>
-            </div>
-          `;
-
-        productContainer.appendChild(productCard);
-      });
+      products = data.products;
+      displayProducts(products);
     })
     .catch((error) => {
       console.error("Error fetching the products:", error);
     });
+
+  const displayProducts = (products) => {
+    productContainer.innerHTML = "";
+    products.forEach((product) => {
+      const productCard = document.createElement("div");
+      productCard.className = "product-card";
+
+      productCard.innerHTML = `
+          <img src="${product.thumbnail}" alt="${product.title}">
+          <div class="product-info">
+            <h2>${product.title}</h2>
+            <p class="description">${product.description}</p>
+            <p class="price">$${product.price}</p>
+            <p class="rating">Rating: ${product.rating} &#9733;</p>
+          </div>
+        `;
+
+      productContainer.appendChild(productCard);
+    });
+  };
+
+  searchBar.addEventListener("keydown", () => {
+    const query = searchBar.value.trim().toLowerCase();
+    if (query) {
+      fetch(`https://dummyjson.com/products/search?q=${query}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const searchedProducts = data.products;
+          displayProducts(searchedProducts);
+        })
+        .catch((error) => {
+          console.error("Error searching for products:", error);
+        });
+    } else {
+      displayProducts(products);
+    }
+  });
 });
