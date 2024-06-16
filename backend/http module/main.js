d9ea947fc3046adc0f847703dfb218f7c3d617f9
NileshGaur <nileshgaur99@gmail.com>
2024-06-16 14:04:48 +0530

    feat: Add card component to pages

--- backend/http module/main.js
+++ backend/http module/main.js
@@ -1,42 +1,122 @@
-const http = require("node:http"); // that signals to Node.js that the http module should be loaded from the core http module in Node.js.
+// const http = require("node:http"); // that signals to Node.js that the http module should be loaded from the core http module in Node.js.
+
+// const app = http.createServer((req, res) => {
+//   console.log("request received", req.url);
+//   console.log(Object.keys(req));
+//   const time = new Date();
+//   res.setHeader("time-of-backend", time.toISOString());
+//   //   res.setHeader("Content-Type", "application/json");
+//   //   res.end(JSON.stringify({ message: "Hello World!" }));
+
+//   res.setHeader("Content-Type", "text/html");
+//   //   res.end(`
+//   //         <html>
+//   //             <head>
+//   //                 <title>My First Page</title>
+//   //                 <style>
+//   //                     body{
+//   //                         background-color: #f0f0f0;
+//   //                         padding: 20px;
+//   //                     }
+//   //                 </style>
+//   //             </head>
+//   //             <body>
+//   //                 <h1>Hello from my Node.js Server!</h1>
+//   //             </body>
+//   //         </html>
+//   //         `);
+
+//   const fsPromises = require("node:fs/promises");
+//   fsPromises.appendFile(
+//     "log.txt",
+//     `Request URL: ${req.url}  ${new Date().toISOString()}\n`
+//   );
+//   fsPromises.readFile("home.html", "utf-8").then((data) => {
+//     res.end(data);
+//   });
+// });
+
+// // 1000 - 9999 is the port number
+// app.listen(2000, () => {
+//   console.log("Server is running on port 2000");
+// });
+
+// --------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------
+
+const http = require("node:http");
+const fsPromises = require("node:fs/promises");
+const url = require("node:url");
+
+const cards = async () => {
+  let cardTemplate = await fsPromises.readFile("./pages/card.html", "utf-8");
+
+  const productsText = await fsPromises.readFile("./data.json", "utf-8");
+  const products = JSON.parse(productsText);
+
+  const resultArray = products.map((ele) => {
+    let cardText = cardTemplate;
+    const title = ele.title;
+    const description = ele.description;
+    const image = ele.thumbnail;
+    cardText = cardText.replace("$cardTitle$", title);
+    cardText = cardText.replace("$cardDescription$", description);
+    cardText = cardText.replace("$imageSrc$", image);
+
+    return cardText;
+  });
+
+  const result = resultArray.join("\n");
+  return result;
+};
+const homePageResponse = async (res) => {
+  const data = await fsPromises.readFile("./pages/homePage.html", "utf-8");
+  res.end(data);
+};
+
+const productsPageResponse = async (res) => {
+  let cardText = await cards();
+  const data = await fsPromises.readFile("./pages/productsPage.html", "utf-8");
+  const productPageHtml = data.replace("#root#", cardText);
+  res.end(productPageHtml);
+};
 
 const app = http.createServer((req, res) => {
-  console.log("request received", req.url);
-  console.log(Object.keys(req));
-  const time = new Date();
-  res.setHeader("time-of-backend", time.toISOString());
-  //   res.setHeader("Content-Type", "application/json");
-  //   res.end(JSON.stringify({ message: "Hello World!" }));
+  const createLogs = (req) => {
+    fsPromises.appendFile(
+      "./log.txt",
+      `Request URL: ${req.url}  ${new Date().toISOString()}\n`
+    );
+  };
+  createLogs(req);
 
   res.setHeader("Content-Type", "text/html");
-  //   res.end(`
-  //         <html>
-  //             <head>
-  //                 <title>My First Page</title>
-  //                 <style>
-  //                     body{
-  //                         background-color: #f0f0f0;
-  //                         padding: 20px;
-  //                     }
-  //                 </style>
-  //             </head>
-  //             <body>
-  //                 <h1>Hello from my Node.js Server!</h1>
-  //             </body>
-  //         </html>
-  //         `);
+  const route = req.url;
+  const { pathname, query } = url.parse(route, true);
 
-  const fsPromises = require("node:fs/promises");
-  fsPromises.appendFile(
-    "log.txt",
-    `Request URL: ${req.url}  ${new Date().toISOString()}\n`
-  );
-  fsPromises.readFile("home.html", "utf-8").then((data) => {
-    res.end(data);
-  });
+  switch (pathname) {
+    case "/": {
+      homePageResponse(res);
+      break;
+    }
+    case "/products": {
+      productsPageResponse(res);
+      break;
+    }
+    default: {
+      res.end("404 Page not found");
+      break;
+    }
+  }
 });
-
-// 1000 - 9999 is the port number
 app.listen(2000, () => {
   console.log("Server is running on port 2000");
 });
